8c5dc97b82b309936909b93d8c8c62d7ec01b292
Goran Miric <goran.miric@etondigital.com>
2014-12-02 11:18:44 +0100

    page template update

--- clean_base/templates/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer id="page-footer">
-  
-  <?php if ($page['footer']): ?>
-    <?php print render($page['footer']); ?>
-  <?php endif; ?>
-  
-</footer>
--- clean_base/templates/page.tpl.php
+++ clean_base/templates/page.tpl.php
@@ -1,7 +1,50 @@
 <div class="page-main">
 
   <!-- Header -->
-  <?php require_once('includes/header.php'); ?>
+  <header id="page-header">
+    <div class="page-holder">
+
+      <!-- Logo -->
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" width="" height="" />
+        </a>
+      <?php endif; ?>
+
+
+      <!-- Region Header -->
+      <?php print render($page['header']); ?>
+
+
+      <!-- Main menu-->
+      <?php if ($main_menu): ?>
+        <nav id="main-menu" class="navigation">
+          <?php print theme('links__system_main_menu', array(
+            'links' => $main_menu,
+            'attributes' => array(
+              'id' => 'main-menu-links',
+              'class' => array('links', 'clearfix'),
+            ),
+          )); ?>
+        </nav>
+      <?php endif; ?>
+
+
+      <!-- Secondary menu -->
+      <?php if ($secondary_menu): ?>
+        <div id="secondary-menu" class="navigation">
+          <?php print theme('links__system_secondary_menu', array(
+            'links' => $secondary_menu,
+            'attributes' => array(
+              'id' => 'secondary-menu-links',
+              'class' => array('links', 'inline', 'clearfix'),
+            ),
+          )); ?>
+        </div>
+      <?php endif; ?>
+
+    </div> <!-- page-holder -->
+  </header>
 
   <!-- Messages -->
   <?php if ($messages): ?>
@@ -63,5 +106,9 @@
 
 
 <!-- Footer -->
-<?php require_once('includes/footer.php'); ?>
+<footer id="page-footer">
+  <?php if ($page['footer']): ?>
+    <?php print render($page['footer']); ?>
+  <?php endif; ?>
+</footer>
 
