b89a8ba51ae7ce1a3a0bf16071ad8c97de6f9cf1
dexterns88 <dexterns88@gmail.com>
2015-06-16 14:56:38 +0200

    changed viewport to prevent user and devices to zoomin page

--- scalar/templates/html.tpl.php
+++ scalar/templates/html.tpl.php
@@ -4,7 +4,7 @@
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
